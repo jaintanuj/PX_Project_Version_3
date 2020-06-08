ddd750a96e721cd2f218b933ebe989645e786e50
tanujjain <aarav@DESKTOP-RRUBKS7>
2020-06-09 07:46:38 +1000

    Recheclking everything done so far

--- PX_Project_Version_3/Pages/AllThemes.cshtml.cs
+++ PX_Project_Version_3/Pages/AllThemes.cshtml.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using PX_Project_Version_3.Data;
 using PX_Project_Version_3.Models;
@@ -22,6 +23,7 @@ namespace PX_Project_Version_3.Pages
 
         public IList<Theme> Theme { get;set; }
         public IList<Event> allEvents { get; set; }
+        public List<SelectListItem> AllEvents { get; set; }
         public bool isAdmin { get; set; }
 
         public async Task<IActionResult> OnGetAsync()
@@ -46,9 +48,60 @@ namespace PX_Project_Version_3.Pages
             //It is because event Code All has been created to just show all events
             allEvents = await _context.Event.Where(e => e.EventCode!="All").ToListAsync();
 
+            AllEvents = await _context.Event.Select(a => new SelectListItem { 
+                Value = a.EventId.ToString(),
+                Text = a.EventCode
+            }).ToListAsync();
+
             Theme = await _context.Theme.ToListAsync();
 
             return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            string username = HttpContext.Session.GetString("username");
+            AppCondition app = await _context.AppCondition.FirstOrDefaultAsync(app => app.AppConditionId.Equals(1));
+
+
+            if (app.AdminName.Equals(username))
+            {
+                isAdmin = true;
+            }
+            else
+            {
+                isAdmin = false;
+            }
+
+            //It is because event Code All has been created to just show all events
+            allEvents = await _context.Event.Where(e => e.EventCode != "All").ToListAsync();
+
+            AllEvents = await _context.Event.Select(a => new SelectListItem
+            {
+                Value = a.EventId.ToString(),
+                Text = a.EventCode
+            }).ToListAsync();
+
+            var eventid = Int32.Parse(Request.Form["eventid"]);
+
+            Event eve = await _context.Event.FirstOrDefaultAsync(e => e.EventId.Equals(eventid));
+
+            if (eve == null)
+            {
+                return RedirectToPage("Privacy");
+            }
+
+            if (eve.EventCode == "All")
+            {
+                Theme = await _context.Theme.ToListAsync();
+            }
+            else
+            {
+                Theme = await _context.Theme.Where(t => t.EventID.Equals(eve.EventId)).ToListAsync();
+            }
+
+            return Page(); 
+
         }
     }
 }
